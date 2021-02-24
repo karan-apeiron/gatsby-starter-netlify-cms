5a4e91fbcc4048091c42bcd0e4b9a94dd7eef948
Karan Dhillon <Karanpal.dhillon@gmail.com>
2021-02-24 15:23:40 +0530

    links fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,11 +39,11 @@ const Footer = class extends React.Component {
                         Products
                       </Link>
                     </li>
-                    <li>
+                    {/* <li>
                       <Link className="navbar-item" to="/contact/examples">
                         Form Examples
                       </Link>
-                    </li>
+                    </li> */}
                     <li>
                       <a
                         className="navbar-item"
